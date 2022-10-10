c538f7a969d20ed224bfd61a83d3b46628d37052
Joshua Clanton <joshua.clanton@gmail.com>
2022-10-10 11:13:59 -0400

    INIT WIP

--- go.mod
+++ go.mod
@@ -30,7 +30,8 @@ require (
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/spf13/viper v1.13.0 // indirect
 	github.com/subosito/gotenv v1.4.1 // indirect
-	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
+	golang.org/x/exp v0.0.0-20221006183845-316c7553db56 // indirect
+	golang.org/x/sys v0.0.0-20220722155257-8c9f86f7a55f // indirect
 	golang.org/x/text v0.3.7 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
--- src/cmd/init.go
+++ src/cmd/init.go
@@ -7,9 +7,11 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+	"regexp"
 
 	"github.com/c-bata/go-prompt"
 	"github.com/spf13/cobra"
+	"golang.org/x/exp/slices"
 )
 
 type AhkpmJson struct {
@@ -48,6 +50,7 @@ var initCmd = &cobra.Command{
 		packageSpec := AhkpmJson{
 			Name:         cwd,
 			Version:      "0.0.1",
+			License:      "MIT",
 			Author:       Person{},
 			Dependencies: make(map[string]string),
 		}
@@ -58,40 +61,46 @@ var initCmd = &cobra.Command{
 			// TODO: use current directory name as default
 			packageSpec.Name = showPrompt(
 				"What is the name of your package?",
+				validateNothing,
 				prompt.OptionInitialBufferText(packageSpec.Name),
 			)
 
 			packageSpec.Version = showPrompt(
 				"What version is the package? (Using semantic versioning)",
+				validateSemver,
 				prompt.OptionInitialBufferText(packageSpec.Version),
 			)
 
 			packageSpec.Description = showPrompt(
 				"Please enter a brief description of the package",
+				validateRequired,
 				prompt.OptionInitialBufferText(packageSpec.Description),
 			)
 
 			// TODO: Look in git config for default
 			packageSpec.Repository = showPrompt(
 				"What is the URL of the package's git repository? (optional)",
+				validateGitHub,
 				prompt.OptionInitialBufferText(packageSpec.Repository),
 			)
 
 			// TODO: use repo url as default
 			packageSpec.Website = showPrompt(
 				"What is the URL of the package's homepage? (optional)",
+				validateNothing,
 				prompt.OptionInitialBufferText(packageSpec.Website),
 			)
 
 			// TODO: use repo url as default
 			packageSpec.IssueTracker = showPrompt(
 				"What is the URL of the package's bug/issue tracker? (optional)",
+				validateNothing,
 				prompt.OptionInitialBufferText(packageSpec.IssueTracker),
 			)
 
 			// TODO: use MIT as default
 			packageSpec.License = showPrompt(
-				"What license is the package released under? (MIT, Apache, etc.) Must be a valid SPDX license identifier.",
+				"What license is the package released under? (MIT, Apache, etc.) Must either be a valid SPDX license identifier or \"UNLICENSED\".",
 				prompt.OptionInitialBufferText(packageSpec.License),
 			)
 
@@ -139,7 +148,7 @@ func init() {
 	rootCmd.AddCommand(initCmd)
 }
 
-func showPrompt(promptMessage string, options ...prompt.Option) string {
+func showPrompt(promptMessage string, validate Validator, options ...prompt.Option) string {
 	noSuggestionsCompleter := func(d prompt.Document) []prompt.Suggest {
 		return []prompt.Suggest{}
 	}
@@ -159,7 +168,7 @@ func showPrompt(promptMessage string, options ...prompt.Option) string {
 	return result
 }
 
-type Validator func(string) (bool string)
+type Validator func(string) (isValid bool, message string)
 
 func validateNothing(value string) (bool, string) {
 	return true, ""
@@ -171,4 +180,44 @@ func validateRequired(value string) (bool, string) {
 	}
 
 	return true, ""
+}
+
+func validateSemver(value string) (bool, string) {
+	// This regular expression is taken from semver.org
+	isMatch := regexp.MatchString("^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$", value)
+	if isMatch {
+		return true, ""
+	}
+	return false, "This is not a valid semantic version. Please see https://semver.org/"
+}
+
+func validateGitHub(value string) (bool, string) {
+	isMatch := regexp.MatchString("^https:\/\/github\.com\/\w+\/\w+$", value)
+	if isMatch {
+		return true, ""
+	}
+	return false, "This is not a valid semantic version. Please see https://semver.org/"
+}
+
+func buildValidatorFromList(options []string) Validator {
+	return func(value string) (bool, string) {
+		isInOptions := slices.Contains(options, string)
+		if isInOptions {
+			return true, ""
+		}
+		return false, "That is not a valid option"
+	}
+}
+
+func buildMultiValidator(validators []Validator) Validator {
+	return func(value string) (bool, string) {
+		for _, validate := range validators {
+			isValid, message := validate(value)
+			if !isValid {
+				return false, message
+			}
+		}
+
+		return true, ""
+	}
 }
