35f5c166aadb7be3ff7c04da37b677f90cc79b43
Joshua Clanton <joshua.clanton@gmail.com>
2022-12-09 11:39:10 -0500

    Add integration tests for `ahkpm uninstall`

--- src/main_test.go
+++ src/main_test.go
@@ -64,3 +64,43 @@ func TestInstallingNewPackageWithDependency(t *testing.T) {
 	err = os.Remove("ahkpm.lock")
 	assert.Nil(t, err)
 }
+
+func TestUninstallingPackageWithDependency(t *testing.T) {
+	core.NewManifest().SaveToCwd()
+	RootCmd.SetArgs([]string{"install", "gh:joshuacc/mock-ahkpm-package-a"})
+	err := RootCmd.Execute()
+	assert.Nil(t, err)
+
+	// Ensure that the correct files were installed
+	assert.FileExists(t, "ahkpm-modules/github.com/joshuacc/mock-ahkpm-package-a/README.md")
+	assert.FileExists(t, "ahkpm-modules/github.com/joshuacc/mock-ahkpm-package-a/ahkpm.json")
+	assert.FileExists(t, "ahkpm-modules/github.com/joshuacc/mock-ahkpm-package-a/ahkpm-modules/github.com/joshuacc/mock-ahkpm-package-b/README.md")
+
+	RootCmd.SetArgs([]string{"uninstall", "gh:joshuacc/mock-ahkpm-package-a"})
+	err = RootCmd.Execute()
+	assert.Nil(t, err)
+
+	m := core.ManifestFromCwd()
+
+	// Ensure ahkpm.json has no dependencies listed
+	assert.Equal(t, 0, len(m.Dependencies.AsArray()))
+
+	lm, err := core.LockManifestFromCwd()
+	assert.Nil(t, err)
+
+	// Ensure that ahkpm.lock has no resolved dependencies
+	assert.Equal(t, 0, len(lm.Resolved))
+
+	// Ensure that the correct files were uninstalled
+	assert.NoFileExists(t, "ahkpm-modules/github.com/joshuacc/mock-ahkpm-package-a/README.md")
+	assert.NoFileExists(t, "ahkpm-modules/github.com/joshuacc/mock-ahkpm-package-a/ahkpm.json")
+	assert.NoFileExists(t, "ahkpm-modules/github.com/joshuacc/mock-ahkpm-package-a/ahkpm-modules/github.com/joshuacc/mock-ahkpm-package-b/README.md")
+
+	// Cleanup
+	err = os.RemoveAll("ahkpm-modules")
+	assert.Nil(t, err)
+	err = os.Remove("ahkpm.json")
+	assert.Nil(t, err)
+	err = os.Remove("ahkpm.lock")
+	assert.Nil(t, err)
+}
